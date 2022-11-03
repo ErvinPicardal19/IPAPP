e0e318fde605fc004f17410a3334fb93ced418ff
Ervin Picardal <ervinjohnpicardal@gmail.com>
2022-11-04 01:55:47 +0800

    Unittest started

--- server.py
+++ server.py
@@ -81,4 +81,4 @@ def printBackLog():
 
 
 if __name__ == "__main__":
-   app.run(host="0.0.0.0", port=PORT)
+   app.run(host="0.0.0.0", port=PORT, debug=True)
---
+++ test.py
@@ -0,0 +1,20 @@
+import unittest
+import requests
+
+class ApiTest(unittest.TestCase):
+   BASE_URL = "http://localhost:3000"
+   TEST_IP = {
+      "data": '8.8.8.8'
+   }
+   
+   def test_home(self):
+      r = requests.get(ApiTest.BASE_URL)
+      self.assertEqual(r.status_code, 200)
+      
+   def test_get_ip_info(self):
+      r = requests.post(f'{ApiTest.BASE_URL}/ip/info', json=ApiTest.TEST_IP)
+      self.assertEqual(r.status_code, 200)
+      
+
+if __name__ == '__main__':
+   unittest.main()
