827b4f469bcee170eb4325ad0d108555c9758cc5
Ervin Picardal <ervinjohnpicardal@gmail.com>
2022-11-05 18:54:25 +0800

    Added requirements.txt and changed build.sh

--- build.sh
+++ build.sh
@@ -1,31 +1,6 @@
 #! bin/bash
 
-mkdir build
-mkdir build/logs
-mkdir build/templates
-mkdir build/static
-
-cp -r logs/* build/logs/.
-cp -r templates/* build/templates/.
-cp -r static/* build/static/.
-cp server.py build/.
-cp logger.py build/.
-
-echo "FROM jenkins/agent:alpine-jdk11" >> build/Dockerfile
-echo "USER root" >> build/Dockerfile
-echo "RUN apk add python3" >> build/Dockerfile
-echo "RUN apk add py3-pip" >> build/Dockerfile
-echo "USER jenkins" >> build/Dockerfile
-echo "RUN pip install flask flask_cors requests" >> build/Dockerfile
-echo "COPY ./logs /home/IPAPP/logs/" >> build/Dockerfile
-echo "COPY ./templates /home/IPAPP/templates/" >> build/Dockerfile
-echo "COPY ./static /home/IPAPP/static/" >> build/Dockerfile
-echo "COPY server.py /home/IPAPP/" >> build/Dockerfile
-echo "COPY logger.py /home/IPAPP/" >> build/Dockerfile
-echo "EXPOSE 3000" >> build/Dockerfile
-echo "CMD python3 /home/IPAPP/server.py" >> build/Dockerfile
-
-cd build
-docker build -t ipapp .
-docker run -t -d -p 3000:3000 --name breakout ipapp
-docker ps -a
+python3 --version
+pip -r i requirements.txt
+pip install -r requirements.txt
+python3 server.py
